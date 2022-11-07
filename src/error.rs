6a945cbe00fb0e8573a85179c14ec96c0a554c61
Silvan Schmidt <silvan.genius@gmail.com>
2022-11-07 07:17:01 +0100

    Rewrite error

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,22 @@
-use thiserror::Error;
-
 pub type GenericError = Box<dyn std::error::Error>;
 
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("error: {0}")]
-    Generic(String),
+#[derive(Debug)]
+pub struct Error {
+    msg: String,
+}
+
+impl Error {
+    pub fn new(msg: &str) -> Error {
+        Error {
+            msg: msg.to_string(),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error: {}", self.msg)
+    }
 }
